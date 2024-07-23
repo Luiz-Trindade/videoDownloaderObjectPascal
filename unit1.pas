4a8012a17f28c6991988cffcd761b03af8ae9cdc
Luiz Trindade <149267777+Luiz-Trindade@users.noreply.github.com>
2024-07-22 21:24:34 -0300

    Update unit1.pas

--- unit1.pas
+++ unit1.pas
@@ -1,3 +1,21 @@
+(*
+    videoDownloader escrito em object-pascal
+    Copyright (C) <2024>  <Luiz Gabriel Magalhães Trindade>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*)
+
 unit Unit1;
 
 {$mode objfpc}{$H+}
